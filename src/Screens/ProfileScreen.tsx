7ae6a7494556521bb3b4c5e9904c6c7f88face5a
marcosvaleriofilho <marcosvaleriofilho@outlook.com>
2024-09-09 10:59:25 -0300

    feat: profilescreen

--- src/Screens/ProfileScreen.tsx
+++ src/Screens/ProfileScreen.tsx
@@ -1,11 +1,30 @@
 // src/Screens/HomeTab.tsx
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Theme } from '../../constants/Theme';
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text>ProfileScreen</Text>
+      <View style={styles.setting}>
+        <Text style={styles.text}>Your Profile</Text>
+      </View>
+      <View style={[{borderBottomWidth: 1, borderColor: Theme.INPUT } ,styles.setting]}>
+        <Text style={[styles.text, {fontFamily: 'Poppins-SemiBold', fontSize: 16}]}>Settings</Text>
+      </View>
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttontext}>E-mail</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttontext}>Name</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttontext}>Change Password</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button,{justifyContent: 'center', borderBottomWidth: 0} ]}>
+          <Text style={[styles.buttontext, {color: 'red', fontFamily: "Poppins-SemiBold"}]}>Log Out</Text>
+        </TouchableOpacity>
+      
     </View>
   );
 }
@@ -16,6 +35,32 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         position: 'relative',
+      },
+    text: {
+        fontSize: 20,
+        fontFamily: 'Poppins-Bold',
+        color: Theme.TERTIARY
     },
+    setting: {
+      flexDirection:'row',
+      justifyContent: 'space-between',
+      top: 0,
+      padding: 16,
+      width: '100%',
+      height: 60,
+    },
+    button: {
+      padding: 24,
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      width: '100%',
+      borderBottomWidth: 1,
+      borderColor: Theme.INPUT,
+    },
+    buttontext:{
+      fontFamily: "Poppins-Regular",
+      fontSize: 16
+    }
 });
 
